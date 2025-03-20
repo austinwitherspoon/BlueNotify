dcb17188d7beeca253ef73714d55604500cffc3f
Austin Witherspoon <46503991+austinwitherspoon@users.noreply.github.com>
2025-03-19 22:32:05 -0700

    Enable IOS notification images

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Austin Witherspoon on 3/19/25.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
